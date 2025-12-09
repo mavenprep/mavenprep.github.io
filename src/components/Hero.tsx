11075015cb1c886e1fbd7fe10aff4c145b9b6425
maven <anjouliu7@gmail.com>
2025-12-09 17:22:37 +0800

    Optimize logo size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,7 +28,7 @@ export function Hero() {
             <img 
               src={mavenLogo} 
               alt="知途 Maven Consulting" 
-              className="h-40 md:h-48 mx-auto"
+              className="h-48 md:h-56 mx-auto"
             />
           </div>
           
